73e457fce4f2340f0971ba31cfbf2032ba365eb9
Shrimad-Bhagwat <sybbhagwat010102@gmail.com>
2021-10-28 17:22:35 +0530

    deploy

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ag^6t1wm35n9msea_9(7shxbd$3dgzu)+oe_)148mjh0w))&9f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['simple-house.herokuapp.com','127.0.0.1']
 
 
 # Application definition
